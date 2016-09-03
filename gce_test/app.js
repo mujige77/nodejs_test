4b7b54589d3ba4ea193bc46b2f71e590b57c2992
tteogi82 <tteogi@daum.net>
2016-09-03 18:55:04 +0900

    테스트용 소스 추가

---
+++ gce_test/app.js
@@ -0,0 +1,49 @@
+'use strict'
+
+var Rx = require('rxjs/Rx')
+var http = require('http').Server()
+var io = require('socket.io')(http)
+var ioCient = require('socket.io-client')
+
+var sockets = []
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    sockets.add(socket);
+
+    socket.on('message', function(msg){
+        console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        var pos = sockets.indexOf(socket);
+        sockets.slice(pos, 1)
+    });
+});
+
+http.listen(3000, '10.140.0.6', function(){
+    console.log('listening on *:3000');
+});
+
+var foo = Rx.Observable.interval(1000)
+foo.subscribe(
+    (x) => {
+        sockets.forEach((socket) => {
+            socket.emit('message',"gogogogogo" + x);
+        })
+    },
+    (err) => { console.log('error ' + err) },
+    () => { console.log('done') }
+)
+
+
+var socket = ioCient('http://10.140.0.7');
+socket.on('message', function(data){
+    console.log('client receive: message' + data)
+});
+
+var socket2 = ioCient('http://10.140.0.8');
+socket2.on('message', function(data){
+    console.log('client receive: message' + data)
+});

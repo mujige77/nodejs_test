946a7d121a13e0a5f6b427e81c4f675476665a5d
tteogi82 <tteogi@daum.net>
2016-09-11 09:00:17 +0900

    Actions, Stores, and Reducers 까지 제작

---
+++ Building Applications with React and Redux in ES6/src/components/course/CoursesPage.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {browserHistory} from 'react-router';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+      </div>
+    )
+  }
+}
+
+export default CoursesPage
